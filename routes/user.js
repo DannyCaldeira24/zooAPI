6d9954b9e7562bc4f9480700448b1a5c7255d932
Danny Caldeira <dannyelportu2013@gmail.com>
2018-09-28 02:06:29 -0400

    Listo obtener cuidadores de los animales

--- routes/user.js
+++ routes/user.js
@@ -13,6 +13,7 @@ api.get('/pruebas-del-controlador', md_auth.ensureAuth, UserController.pruebas);
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 api.post('/upload-image/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
 api.get('/get-image/:imageFile', UserController.getImageFile);
+api.get('/get-keepers', UserController.getKeepers);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.login);
 
