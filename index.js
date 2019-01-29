4d3c65785b5d879db03e09681103885643024858
Danny Caldeira <dannyelportu2013@gmail.com>
2019-01-29 17:47:20 -0400

    prod

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
   
 const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/zoo_db';
-var PORT = process.env.PORT || 0;
-
+// var PORT = process.env.PORT || 0;
+app.set('port', process.env.PORT || 3000);
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URI, { useNewUrlParser: true })
 		.then(()=>{
 			console.log('La conexión a la base de datos zoo se ha realizado correctamente..');
-			let server = app.listen(PORT, () => {
+			let server = app.listen(app.get('port'), () => {
 			    var host = server.address().address;
 			    var port = server.address().port;
 			    console.log('http://localhost:%s', port);
